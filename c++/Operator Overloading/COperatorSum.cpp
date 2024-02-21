05db2ffe13e44e6c3efe07a0111eedc68c1ba9c4
chetansq <sq.chetan@gmail.com>
2024-02-21 08:36:48 +0530

    practice

--- c++/Operator Overloading/COperatorSum.cpp
+++ c++/Operator Overloading/COperatorSum.cpp
@@ -1,61 +1,108 @@
 #include <iostream>
 using namespace std;
 
-class arith_num
+// class arith_num
+// {
+//     int x, y;
+
+// public:
+//     arith_num()
+//     {
+//         x = 0;
+//         y = 0;
+
+//     }
+
+//     arith_num(int a, int b)
+//     {
+//         x = a;
+//         y = b;
+
+//     }
+
+//     void print(int a)
+//     {
+//         cout << "the " << a << " value of x is : " << x << endl;
+//         cout << "the " << a << " value of y is : " << y << endl;
+//     }
+
+//     arith_num operator+(arith_num obj)
+//     {
+//         arith_num A;
+
+//         A.x = x + obj.x;
+//         A.y = y + obj.y;
+
+//         return A;
+//     }
+// };
+
+// int main()
+// {
+
+//     arith_num a, b, c;
+
+//     a = arith_num(10, 20);
+//     b = arith_num(0,40);
+
+//     // cout << "the value of a " << endl;
+//     a.print(1);
+//     cout << endl;
+//     // cout << "the value of b  " << endl;
+//     b.print(2);
+//     cout << endl;
+//     c = a + b;
+//     // cout << "total " << endl;
+//     c.print(3);
+//     return 0;
+// }
+
+class word
 {
-    int x, y;
+    int a, b;
 
 public:
-    arith_num()
+    word()
     {
-        x = 0;
-        y = 0;
-
-        // cout << "enter value of a : ";
-        // cin >> a;
+        a = 0;
+        b = 0;
     }
 
-    arith_num(int a, int b)
+    word(int x, int y)
     {
-        x = a;
-        y = b;
-        // cout << "enter value of b : ";
-        // cin >> b;
+        a = x;
+        b = y;
     }
 
-    void print(int a)
+    void print(char x)
     {
-        cout << "the " << a << " value of x is : " << x << endl;
-        cout << "the " << a << " value of y is : " << y << endl;
+        cout << "Value " << x << " is : " << a << endl;
+        cout << "value " << x << " is : " << b << endl;
     }
 
-    arith_num operator+(arith_num obj)
+    word operator/(word ob)
     {
-        arith_num A;
-
-        A.x = x + obj.x;
-        A.y = y + obj.y;
-
-        return A;
+        word w;
+        w.a = a / ob.a;
+        w.b = b / ob.b;
+        return w;
     }
 };
 
 int main()
 {
+    word a, b, c;
 
-    arith_num a, b, c;
+    a = word(10, 0);
+    b = word(10, 20);
 
-    a = arith_num(10, 20);
-    b = arith_num(0,40);
-
-    // cout << "the value of a " << endl;
-    a.print(1);
+    a.print('A');
     cout << endl;
-    // cout << "the value of b  " << endl;
-    b.print(2);
+
+    b.print('B');
     cout << endl;
-    c = a + b;
-    // cout << "total " << endl;
-    c.print(3);
+
+    c = a / b;
+    c.print('C');
     return 0;
 }

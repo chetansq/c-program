6263d07838e7a485f5286e7d8fe5ab76467f806a
chetansq <sq.chetan@gmail.com>
2024-02-26 09:06:51 +0530

    multiple

---
+++ c++/Inheritance/Multiple/multiple.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+    int id;
+
+public:
+    void id_A()
+    {
+        cout << "etner id From calss A :";
+        cin >> id;
+    }
+
+    void print1()
+    {
+        cout << "id From class A: " << id << endl;
+    }
+};
+
+class B
+{
+    int id;
+
+public:
+    void id_B()
+    {
+        cout << "etner id From calss B :";
+        cin >> id;
+    }
+
+    void print2()
+    {
+        cout << "id From class B : " << id << endl;
+    }
+};
+
+class C : public A, public B
+{
+    char name[20];
+
+public:
+    void n_C()
+    {
+        cout << "etner Name :";
+        cin >> name;
+    }
+
+    void print()
+    {
+        cout << endl;
+        print1();
+        print2();
+        cout << "Name is : " << name << endl;
+    }
+};
+int main()
+{
+
+    C c1;
+    c1.id_A();
+    c1.id_B();
+    c1.n_C();
+    c1.print();
+    return 0;
+}
